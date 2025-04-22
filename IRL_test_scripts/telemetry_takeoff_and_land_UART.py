fa2d9ff692335d04313ed378e603dfa8d4963577
tonyjohns0n <anthonyjohnson@ucsb.edu>
2025-04-22 11:51:41 -0700

    Test commit from personal laptop

--- IRL_test_scripts/telemetry_takeoff_and_land_UART.py
+++ IRL_test_scripts/telemetry_takeoff_and_land_UART.py
@@ -26,7 +26,6 @@ async def run():
 
     # Init the drone
     drone = System()
-    # await drone.connect(system_address="udp://:14540")
     await drone.connect(system_address="serial:///dev/ttyAMA0:57600")
 
     print("Waiting for drone to connect...")
